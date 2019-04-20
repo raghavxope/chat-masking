ed7fce34a29440923563dc932032ffd7d2c3fd9b
raghavxope <akileshbabu1997@gmail.com>
2019-04-20 14:10:35 +0530

    Create encoding.py

---
+++ encoding.py
@@ -0,0 +1,50 @@
+#encoding tecnique
+chara=raw_input("enter a sentence")
+print(type(chara))
+thisdic={
+  "a":"apple",
+  "b":"bag",
+  "c":"carrot",
+  "d":"dodge",
+  "e":"eat",
+  "f":"fat",
+  "g":"garbage",
+  "h":"hot",
+  "i":"ice",
+  "j":"jackfruit",
+  "k":"k",
+  "l":"liquid",
+  "m":"man",
+  "n":"narcotics",
+  "o":"or",
+  "p":"paste",
+  "q":"quater",
+  "r":"rough",
+  "s":"sharp",
+  "t":"tastes",
+  "u":"you",
+  "v":"we",
+  "x":"no",
+  "y":"why",
+  "z":"zzzz",
+  " ":" "
+}
+elist=[]
+
+nlist=[]
+
+pos=0
+
+while pos!=len(chara):
+    gg=chara[pos]
+   # elist.insert[pos,gg]
+    pos=pos+1
+
+p=0
+while p!=len(elist):
+    nlist[p]=thisdic.get(elist[p])
+    p=p+1
+for x in nlist:
+    print(x)
+
+
